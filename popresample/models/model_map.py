a9b3fdfba85c7b35a44b06522ad8e37576fce5b7
Christian Adamcewicz <christian.adamcewicz@ldas-pcdev2.ligo.caltech.edu>
2023-08-31 17:46:23 -0700

    cleaning

--- popresample/models/model_map.py
+++ popresample/models/model_map.py
@@ -5,13 +5,8 @@ from gwpopulation.models.mass import SinglePeakSmoothedMassDistribution
 from gwpopulation.models.spin import iid_spin
 from gwpopulation.models.redshift import PowerLawRedshift
 
-from .joint import MassRedshiftCopula, SPSMD_EffectiveCopula
-
-
 MODEL_MAP = {
     "SinglePeakSmoothedMassDistribution":SinglePeakSmoothedMassDistribution,
     "iid_spin":iid_spin,
-    "PowerLawRedshift":PowerLawRedshift,
-    "MassRedshiftCopula":MassRedshiftCopula,
-    "SPSMD_EffectiveCopula":SPSMD_EffectiveCopula
+    "PowerLawRedshift":PowerLawRedshift
 }
